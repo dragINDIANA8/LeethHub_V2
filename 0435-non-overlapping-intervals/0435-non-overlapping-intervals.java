f066950cc4d54d4f4c5f32a682d0e9e53b4bb5fc
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2025-05-18 14:16:20 +0530

    Time: 49 ms (45.33%), Space: 73.6 MB (47.43%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,20 +3,19 @@ class Solution {
      if (intervals.length == 0) {
             return 0;
         }
-        Arrays.sort(intervals, (a, b)-> Integer.compare(a[1], b[1]));
+        Arrays.sort(intervals, (a, b)->Integer.compare(a[1], b[1]));
 
         int end = intervals[0][1];
-        int count = 0;
+        int count = 1;
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < end) {
+            if (intervals[i][0] >= end) {
         
                 count++;
-            } else {
-               end = intervals[i][1];
+                end = intervals[i][1];
             }
         }
         
-        return count;
+        return intervals.length - count;
     }
 
     }
